0ecdd6ca05db290a05ac1ca760322f5328f40b58
cyoonUCSD <103143169+cyoonUCSD@users.noreply.github.com>
2022-04-22 17:02:49 -0700

    Add files via upload

---
+++ MarkdownParseTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MarkdownParseTest {
+
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void ParseTest() throws IOException
+    {
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(links, List.of("https://something.com", "some-thing.html"));
+    }
+}
